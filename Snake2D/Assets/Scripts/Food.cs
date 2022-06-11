68e7fae742174f2b673a39e7cf2d28a589d33202
Tuğrul Sübekci <104980354+tugrulsubekci@users.noreply.github.com>
2022-06-11 20:26:53 +0300

    Creating a Snake Game

---
+++ Snake2D/Assets/Scripts/Food.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class Food : MonoBehaviour
+{
+    private void Start()
+    {
+        RandomizePosition();
+    }
+
+    private void Update()
+    {
+
+    }
+
+    public BoxCollider2D gridArea;
+    private void RandomizePosition()
+    {
+        Bounds gridAreaBounds = gridArea.bounds;
+
+        float x = Mathf.Round(Random.Range(gridAreaBounds.min.x,gridAreaBounds.max.x));
+        float y = Mathf.Round(Random.Range(gridAreaBounds.min.y,gridAreaBounds.max.y));
+
+        transform.localPosition = new Vector3(x,y,0.0f);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            RandomizePosition();
+        } 
+    }
+}
---
+++ Snake2D/Assets/Scripts/SnakeMovement.cs
@@ -0,0 +1,111 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeMovement : MonoBehaviour
+{
+    public List<Transform> _segments = new List<Transform>();
+    public Transform segmentPrefab;
+    public int initialSize = 4;
+    public Vector2 direction = Vector2.right;
+
+    // turning head
+    public Vector3 rightHead = new Vector3(0, 0, 0);
+    public Vector3 upHead = new Vector3(0, 0, 90);
+    public Vector3 leftHead = new Vector3(0, 0, 180);
+    public Vector3 downHead = new Vector3(0, 0, 270);
+
+
+    private void Start()
+    {
+        ResetState();
+        this.transform.localEulerAngles = rightHead; // turning head
+    }
+
+    private void Update()
+    {
+        // Snake Head Movement
+        if (this.direction.x != 0.0f)
+        {
+            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                this.direction = Vector2.up;
+                this.transform.localEulerAngles = upHead; // turning head
+            }
+            else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                this.direction = Vector2.down;
+                this.transform.localEulerAngles = downHead; // turning head
+            }
+        }
+
+        else if (this.direction.y != 0.0f)
+        {
+            if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                this.direction = Vector2.right;
+                this.transform.localEulerAngles = rightHead; // turning head
+            }
+            else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                this.direction = Vector2.left;
+                this.transform.localEulerAngles = leftHead; // turning head
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // Snake Body positioning
+        for (int i = _segments.Count - 1; i > 0; i--)
+        {
+            _segments[i].position = _segments[i - 1].position;
+        }
+        // Snake Head Movement
+        this.transform.position = new Vector3(
+            Mathf.Round(this.transform.position.x) + this.direction.x,
+            Mathf.Round(this.transform.position.y) + this.direction.y);
+    }
+
+    // Snake Growing
+    public void Grow()
+    {
+        Transform segment = Instantiate(this.segmentPrefab);
+        segment.position = _segments[_segments.Count - 1].position;
+        _segments.Add(segment);
+    }
+
+    // Game Over
+    public void ResetState()
+    {
+        this.direction = Vector2.right;
+        this.transform.position = Vector3.zero;
+
+        for (int i = 1; i < _segments.Count; i++)
+        {
+            Destroy(_segments[i].gameObject);
+        }
+
+        _segments.Clear();
+        _segments.Add(this.transform);
+
+        for (int i = 0; i < this.initialSize - 1; i++)
+        {
+            Grow();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Food")
+        {
+            Grow();
+            FindObjectOfType<AudioManager>().Play("Eating");
+        }
+
+        else if (other.tag == "Obstacle")
+        {
+            ResetState();
+            FindObjectOfType<AudioManager>().Play("GameOver");
+        }
+    }
+}
